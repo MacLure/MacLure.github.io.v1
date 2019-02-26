baf3eb5ba188c686c35e0f1ccf6c4d7a3c53dccb
Malcolm <malcolm.maclure@gmail.com>
2019-02-25 22:51:04 -0500

    3d transform on mousemove over project links

--- app.js
+++ app.js
@@ -37,3 +37,89 @@ sass.addEventListener('mouseout', function () {sassText.innerText=('')})
 ruby.addEventListener('mouseout', function () {rubyText.innerText=('')})
 rails.addEventListener('mouseout', function () {railsText.innerText=('')})
 git.addEventListener('mouseout', function () {gitText.innerText=('')})
+
+// diamond transformations
+
+const diamond1 = document.querySelector('.diamond1')
+const diamond2 = document.querySelector('.diamond2')
+const diamond3 = document.querySelector('.diamond3')
+const diamond4 = document.querySelector('.diamond4')
+
+const walk = 15;
+
+function move1(e) {
+  const { offsetWidth: width, offsetHeight: height } = diamond1;
+  let {offsetX: x, offsetY: y} = e;
+  if(this !==e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
+  }
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+  diamond1.style.transform = `perspective(400px) RotateZ(45deg) rotateY(${yWalk}deg) rotateX(${xWalk}deg)`
+
+}
+
+function reset1() {
+  diamond1.style.transform = `Rotate(45deg)`
+}
+
+function move2(e) {
+  const { offsetWidth: width, offsetHeight: height } = diamond1;
+  let {offsetX: x, offsetY: y} = e;
+  if(this !==e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
+  }
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+  diamond2.style.transform = `perspective(400px) RotateZ(45deg) rotateY(${yWalk}deg) rotateX(${xWalk}deg)`
+}
+
+function reset2() {
+  diamond2.style.transform = `Rotate(45deg)`
+}
+
+function move3(e) {
+  const { offsetWidth: width, offsetHeight: height } = diamond1;
+  let {offsetX: x, offsetY: y} = e;
+  if(this !==e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
+  }
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+  diamond3.style.transform = `perspective(400px) RotateZ(45deg) rotateY(${yWalk}deg) rotateX(${xWalk}deg)`
+}
+
+function reset3() {
+  diamond3.style.transform = `Rotate(45deg)`
+}
+
+function move4(e) {
+  const { offsetWidth: width, offsetHeight: height } = diamond1;
+  let {offsetX: x, offsetY: y} = e;
+  if(this !==e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
+  }
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+  diamond4.style.transform = `perspective(400px) RotateZ(45deg) rotateY(${yWalk}deg) rotateX(${xWalk}deg)`
+}
+
+function reset4() {
+  diamond4.style.transform = `Rotate(45deg)`
+}
+
+diamond1.addEventListener('mousemove', move1);
+diamond1.addEventListener('mouseleave', reset1);
+
+diamond2.addEventListener('mousemove', move2);
+diamond2.addEventListener('mouseleave', reset2);
+
+diamond3.addEventListener('mousemove', move3);
+diamond3.addEventListener('mouseleave', reset3);
+
+diamond4.addEventListener('mousemove', move4);
+diamond4.addEventListener('mouseleave', reset4);
