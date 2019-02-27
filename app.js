c91e5ab2424298713f24d1dd9abb2335563abe2c
Malcolm <malcolm.maclure@gmail.com>
2019-02-27 15:40:08 -0500

    removed glitchiness on project diamonds

--- app.js
+++ app.js
@@ -52,81 +52,75 @@ const diamond2 = document.querySelector('.diamond2')
 const diamond3 = document.querySelector('.diamond3')
 const diamond4 = document.querySelector('.diamond4')
 
-const walk = 15;
+const walk = 50;
 
 function move1(e) {
-  const { offsetWidth: width, offsetHeight: height } = diamond1;
+  const { offsetWidth: width, offsetHeight: height } = diamond1Activator;
   let {offsetX: x, offsetY: y} = e;
-  if(this !==e.target) {
-      x = x + e.target.offsetLeft;
-      y = y + e.target.offsetTop;
-  }
   const xWalk = Math.round((x / width * walk) - (walk / 2));
   const yWalk = Math.round((y / height * walk) - (walk / 2));
+  diamond1.classList.add("gradientHover")
   diamond1.style.transform = `perspective(400px) RotateZ(45deg) rotateY(${yWalk}deg) rotateX(${xWalk}deg)`
-
 }
 
 function reset1() {
+  diamond1.classList.remove("gradientHover")
   diamond1.style.transform = `Rotate(45deg)`
 }
 
 function move2(e) {
-  const { offsetWidth: width, offsetHeight: height } = diamond1;
+  const { offsetWidth: width, offsetHeight: height } = diamond2Activator;
   let {offsetX: x, offsetY: y} = e;
-  if(this !==e.target) {
-      x = x + e.target.offsetLeft;
-      y = y + e.target.offsetTop;
-  }
   const xWalk = Math.round((x / width * walk) - (walk / 2));
   const yWalk = Math.round((y / height * walk) - (walk / 2));
+  diamond2.classList.add("gradientHover")
   diamond2.style.transform = `perspective(400px) RotateZ(45deg) rotateY(${yWalk}deg) rotateX(${xWalk}deg)`
 }
 
 function reset2() {
+  diamond2.classList.remove("gradientHover")
   diamond2.style.transform = `Rotate(45deg)`
 }
-
 function move3(e) {
-  const { offsetWidth: width, offsetHeight: height } = diamond1;
+  const { offsetWidth: width, offsetHeight: height } = diamond3Activator;
   let {offsetX: x, offsetY: y} = e;
-  if(this !==e.target) {
-      x = x + e.target.offsetLeft;
-      y = y + e.target.offsetTop;
-  }
   const xWalk = Math.round((x / width * walk) - (walk / 2));
   const yWalk = Math.round((y / height * walk) - (walk / 2));
+  diamond3.classList.add("gradientHover")
   diamond3.style.transform = `perspective(400px) RotateZ(45deg) rotateY(${yWalk}deg) rotateX(${xWalk}deg)`
 }
 
 function reset3() {
+  diamond3.classList.remove("gradientHover")
   diamond3.style.transform = `Rotate(45deg)`
 }
 
 function move4(e) {
-  const { offsetWidth: width, offsetHeight: height } = diamond1;
+  const { offsetWidth: width, offsetHeight: height } = diamond4Activator;
   let {offsetX: x, offsetY: y} = e;
-  if(this !==e.target) {
-      x = x + e.target.offsetLeft;
-      y = y + e.target.offsetTop;
-  }
   const xWalk = Math.round((x / width * walk) - (walk / 2));
   const yWalk = Math.round((y / height * walk) - (walk / 2));
+  diamond4.classList.add("gradientHover")
   diamond4.style.transform = `perspective(400px) RotateZ(45deg) rotateY(${yWalk}deg) rotateX(${xWalk}deg)`
 }
 
 function reset4() {
+  diamond4.classList.remove("gradientHover")
   diamond4.style.transform = `Rotate(45deg)`
 }
+const diamond1Activator = document.querySelector('.diamond1Activator')
+const diamond2Activator = document.querySelector('.diamond2Activator')
+const diamond3Activator = document.querySelector('.diamond3Activator')
+const diamond4Activator = document.querySelector('.diamond4Activator')
 
-diamond1.addEventListener('mousemove', move1);
-diamond1.addEventListener('mouseleave', reset1);
+diamond1Activator.addEventListener('mousemove', move1);
+diamond1Activator.addEventListener('mouseleave', reset1);
 
-diamond2.addEventListener('mousemove', move2);
-diamond2.addEventListener('mouseleave', reset2);
+diamond2Activator.addEventListener('mousemove', move2);
+diamond2Activator.addEventListener('mouseleave', reset2);
 
-diamond3.addEventListener('mousemove', move3);
-diamond3.addEventListener('mouseleave', reset3);
+diamond3Activator.addEventListener('mousemove', move3);
+diamond3Activator.addEventListener('mouseleave', reset3);
 
-diamond4.addEventListener('mousemove', move4);
-diamond4.addEventListener('mouseleave', reset4);
+diamond4Activator.addEventListener('mousemove', move4);
+diamond4Activator.addEventListener('mouseleave', reset4);
