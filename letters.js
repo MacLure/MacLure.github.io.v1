8d18c51f856444d833ee2aedfd40ebdfbc9dea74
Malcolm <malcolm.maclure@gmail.com>
2019-03-02 20:41:36 -0500

    transitinos on hero section

--- letters.js
+++ letters.js
@@ -28,6 +28,18 @@ l6 = document.querySelector('#l6')
 l7t = document.querySelector('#l7t')
 l7 = document.querySelector('#l7')
 
+lineDivider = document.querySelectorAll('.lineDivider')
+heroH2 = document.querySelector('.heroH2')
+
+largeOrangeGear = document.querySelector('.largeOrangeGear')
+smallCenterPinkGear = document.querySelector('.smallCenterPinkGear')
+largeLeftPinkGear = document.querySelector('.largeLeftPinkGear')
+largeRightPinkGear = document.querySelector('.largeRightPinkGear')
+leftBlackGear = document.querySelector('.leftBlackGear')
+rightBlackGearBottom = document.querySelector('.rightBlackGearBottom')
+rightBlackGearRight = document.querySelector('.rightBlackGearRight')
+scrollButton = document.querySelector('.scrollButton')
+
 adjustor = 0.33
 
 f1t.addEventListener('mouseover', function (e) {
@@ -176,21 +188,42 @@ l7t.addEventListener('mouseout', function (e) {
 
 
 window.onload = function() {
-  f1.classList.add("active")
-  setTimeout(function(){f2.classList.add("active")}, 100 * adjustor)
-  setTimeout(function(){f3.classList.add("active")}, 200 * adjustor)
-  setTimeout(function(){f4.classList.add("active")}, 300 * adjustor)
-  setTimeout(function(){f5.classList.add("active")}, 400 * adjustor)
-  setTimeout(function(){f6.classList.add("active")}, 500 * adjustor)
-  setTimeout(function(){f7.classList.add("active")}, 600 * adjustor)
 
-  setTimeout(function(){l1.classList.add("active")}, 700 * adjustor)
-  setTimeout(function(){l2.classList.add("active")}, 800 * adjustor)
-  setTimeout(function(){l3.classList.add("active")}, 900 * adjustor)
-  setTimeout(function(){l4.classList.add("active")}, 1000 * adjustor)
-  setTimeout(function(){l5.classList.add("active")}, 1100 * adjustor)
-  setTimeout(function(){l6.classList.add("active")}, 1200 * adjustor)
-  setTimeout(function(){l7.classList.add("active")}, 1300 * adjustor)
+  // setTimeout(function(){
+  //   lineDivider[0].classList.remove("invisible")
+  //   lineDivider[1].classList.remove("invisible")
+  // }, 0)
+
+  setTimeout(function(){cycleNameLetters() }, 500)
+
+  setTimeout(function(){
+    heroH2.classList.remove("invisible")
+  }, 1300)
+  
+  setTimeout(function(){ showGears() }, 2000)
+  
+  
+}
+
+
+
+
+function cycleNameLetters() {
+  setTimeout(function(){f1.classList.add("active"), f1.classList.remove("invisible")}, 0 * adjustor)
+  setTimeout(function(){f2.classList.add("active"), f2.classList.remove("invisible")}, 100 * adjustor)
+  setTimeout(function(){f3.classList.add("active"), f3.classList.remove("invisible")}, 200 * adjustor)
+  setTimeout(function(){f4.classList.add("active"), f4.classList.remove("invisible")}, 300 * adjustor)
+  setTimeout(function(){f5.classList.add("active"), f5.classList.remove("invisible")}, 400 * adjustor)
+  setTimeout(function(){f6.classList.add("active"), f6.classList.remove("invisible")}, 500 * adjustor)
+  setTimeout(function(){f7.classList.add("active"), f7.classList.remove("invisible")}, 600 * adjustor)
+
+  setTimeout(function(){l1.classList.add("active"), l1.classList.remove("invisible")}, 700 * adjustor)
+  setTimeout(function(){l2.classList.add("active"), l2.classList.remove("invisible")}, 800 * adjustor)
+  setTimeout(function(){l3.classList.add("active"), l3.classList.remove("invisible")}, 900 * adjustor)
+  setTimeout(function(){l4.classList.add("active"), l4.classList.remove("invisible")}, 1000 * adjustor)
+  setTimeout(function(){l5.classList.add("active"), l5.classList.remove("invisible")}, 1100 * adjustor)
+  setTimeout(function(){l6.classList.add("active"), l6.classList.remove("invisible")}, 1200 * adjustor)
+  setTimeout(function(){l7.classList.add("active"), l7.classList.remove("invisible")}, 1300 * adjustor)
 
 
   setTimeout(function(){f1.classList.remove("active")}, 700 * adjustor)
@@ -210,3 +243,19 @@ window.onload = function() {
   setTimeout(function(){l7.classList.remove("active")}, 2000 * adjustor)
 }
 
+
+function showGears() {
+  setTimeout(function(){leftBlackGear.classList.remove("invisible")}, 0 * adjustor)
+  setTimeout(function(){rightBlackGearRight.classList.remove("invisible")}, 500 * adjustor)
+  setTimeout(function(){rightBlackGearBottom.classList.remove("invisible")}, 500 * adjustor)
+
+  setTimeout(function(){largeLeftPinkGear.classList.remove("invisible")}, 1000 * adjustor)
+  setTimeout(function(){largeRightPinkGear.classList.remove("invisible")}, 1500 * adjustor)
+
+  setTimeout(function(){smallCenterPinkGear.classList.remove("invisible")}, 2000 * adjustor)
+
+  setTimeout(function(){largeOrangeGear.classList.remove("invisible")}, 3000 * adjustor)
+
+  setTimeout(function(){scrollButton.classList.remove("invisible")}, 4000 * adjustor)
+
+}
